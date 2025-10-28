3ab8dd698eabf5fdc28c14aeb22f11d8b61efebd
BenLeland <blelan@uw.edu>
2025-10-27 22:38:40 -0700

    Add background for sketch 2

--- sketches/sketch3.js
+++ sketches/sketch3.js
@@ -1,16 +1,50 @@
 // Instance-mode sketch for tab 3
 registerSketch('sk3', function (p) {
+  let rocks = [];
+
   p.setup = function () {
     p.createCanvas(p.windowWidth, p.windowHeight);
+
+    // Generate static rocks
+    rocks = [];
+    for (let i = 0; i < 60; i++) {
+      rocks.push({
+        x: p.random(p.width),
+        y: p.random(p.height),
+        size: p.random(7, 18),
+        gray: p.random(90, 140),
+        rot: p.random(p.TWO_PI),
+        noiseSeed: p.random(1000)
+      });
+    }
   };
 
   p.draw = function () {
-    p.background(255);
-    p.fill(180, 60, 60);
-    p.textSize(32);
-    p.textAlign(p.CENTER, p.CENTER);
-    p.text('HWK #4. B', p.width / 2, p.height / 2);
+    p.background('tan');
+
+    // Draw rocks (static, small, irregular)
+    for (let i = 0; i < rocks.length; i++) {
+      let rock = rocks[i];
+      p.push();
+      p.translate(rock.x, rock.y);
+      p.rotate(rock.rot);
+      p.fill(rock.gray, rock.gray * 0.95, rock.gray * 0.9, 220);
+      p.stroke(rock.gray * 0.7, 120);
+      p.strokeWeight(1);
+      p.beginShape();
+      for (let a = 0; a < p.TWO_PI; a += 0.3) {
+        let rad = rock.size * (0.7 + p.noise(rock.noiseSeed + a * 2, rock.noiseSeed + a * 3) * 0.5);
+        let x = rad * Math.cos(a);
+        let y = rad * Math.sin(a);
+        p.vertex(x, y);
+      }
+      p.endShape(p.CLOSE);
+      p.pop();
+    }
+
   };
-  
-  p.windowResized = function () { p.resizeCanvas(p.windowWidth, p.windowHeight); };
+
+  p.windowResized = function () {
+    p.resizeCanvas(p.windowWidth, p.windowHeight);
+  };
 });
